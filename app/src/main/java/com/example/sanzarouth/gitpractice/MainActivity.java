a69169aad49aee8a771e96b53d8bae7ea8cef687
Sophie Anzarouth <sanzarouth@hotmail.com>
2018-10-11 09:56:47 +0100

    Feature (#1)
    * Button with listener to color background added
    
    * changed button design
    
    * clean

--- app/src/main/java/com/example/sanzarouth/gitpractice/MainActivity.java
+++ app/src/main/java/com/example/sanzarouth/gitpractice/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.sanzarouth.gitpractice;
 
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+
+        final int first = getResources().getColor(R.color.colorAccent);
+        final int second = getResources().getColor(R.color.colorPrimary);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                ColorDrawable buttonColor = (ColorDrawable) view.getBackground();
+                int colorId = buttonColor.getColor();
+
+                if (colorId == first) {
+                   view.setBackgroundColor(second);
+                } else {
+                    view.setBackgroundColor(first);
+                }
+            }
+        });
     }
 }
